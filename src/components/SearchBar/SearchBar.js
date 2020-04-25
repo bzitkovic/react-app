2af3284612d114c9327e3dad2390ca2153de5f42
Bruno Žitković <brunozitkovic@gmail.com>
2020-04-25 21:18:53 +0200

    added styled component on SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './SearchBar.scss';
+import { SearchBarContainer, SearchBarInput } from './SearchBarStyle.js';
 
 const SearchBar = (props) => {
     return (
-        <div className="SearchBar">
-            <input disabled={props.active} type="text" value={props.value} onChange={props.change} className="SearchBar_Input" placeholder={props.placeholder}/>
-        </div>
+        <SearchBarContainer>
+            <SearchBarInput disabled={props.active} type="text" value={props.value} onChange={props.change} placeholder={props.placeholder}/>
+        </SearchBarContainer>
     )
 }
 
--- src/components/SearchBar/SearchBarStyle.js
+++ src/components/SearchBar/SearchBarStyle.js
@@ -1,17 +1,18 @@
-.SearchBar{
-  
+import styled from 'styled-components';
+
+export const SearchBarContainer = styled.div `
     width: 400px;
     position: absolute;
     left: 50%;
     transform: translate(-50%, -50%);
-   
-}
-
-.SearchBar_Input{
+`;
+  
+  
+export const SearchBarInput = styled.input `
     text-align: center;
     width: 400px;
     line-height: 50px;
     border: 1px solid  #cecece;
     border-radius: 6px;
     border-color: #c59613;
-}
+`;
